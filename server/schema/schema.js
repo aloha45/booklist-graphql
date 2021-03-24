cb570c151c15232a3e1cfbdba7020b9a176a0c17
Cory <corys4139@gmail.com>
2021-03-24 12:45:40 -0400

    add book schema and prettier rules

---
+++ server/schema/schema.js
@@ -0,0 +1,14 @@
+const graphql = require("graphql");
+
+const { GraphQLOjectType, GraphQLString } = graphql;
+
+const BookType = new GraphQLOjectType({
+  name: "Book",
+  fields: () => ({
+    id: { type: GraphQLString },
+    name: { type: GraphQLString },
+    genre: { type: GraphQLString },
+  }),
+});
+
+
